d96feebea35e8735534a484fe5ead826529668e2
vgri-z <vgri199712@163.com>
2020-01-31 14:42:26 +0800

    事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,63 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    titles: ['衣服', '鞋子', '帽子']
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    console.log("按钮发生点击")
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTouchStart() {
+    console.log("handleTouchStart")
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleTouchMove() {
+    console.log("handleTouchMove")
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleTouchEnd() {
+    console.log("handleTouchEnd")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleTap() {
+    console.log("handleTap")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  handlelongPress() {
+    console.log("handlelongPress")
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  // -----------------事件对象分析
+  handleEventClick(event) {
+    console.log("--------", event)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleEventEnd(event) {
+    console.log("++++++++", event)
+  },
+  handleOuter(event) {
+    console.log(event)
+  },
+  handleInner(event) {
+    console.log(event)
+  },
+  // -----------事件的传递参数
+  handleItemClikc(event) {
+    console.log(event);
+    const data = event.currentTarget.dataset;
+    console.log(data.index);
+    console.log(data.item);
+  },
+  // ----------------事件冒泡与事件捕获
+  handleCaptureOuter() {
+    console.log("handleCaptureOuter")
+  },
+  handleBulbleOuter() {
+    console.log("handleBulbleOuter")
+  },
+  handleCaptureMiddle() {
+    console.log("handleCaptureMiddle")
+  },
+  handleBulbleMiddle() {
+    console.log("handleBulbleMiddle")
+  },
+  handleCaptureInner() {
+    console.log("handleCaptureInner")
+  },
+  handleBulbleInner() {
+    console.log("handleBulbleInner")
   }
 })
