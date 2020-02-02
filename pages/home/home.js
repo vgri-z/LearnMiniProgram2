3d81a2d630e25ce991683647a6250456faf4e06a
vgri-z <vgri199712@163.com>
2020-02-02 13:31:20 +0800

    网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,59 @@
 // pages/home/home.js
+import request from "../../service/network.js"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  data:{
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // 原生的方式发送网络请求
+    this.get_data_origin()
 
+    // 2.使用封装的request发送请求
+    request({
+      url: "http://106.54.54.237:8000/api/hy/recommend"
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   },
+  get_data_origin() {
+    // 发送网络请求
+    // 1.发送简单地get请求
+    // wx.request({
+    //   url: 'http://106.54.54.237:8000/api/hy/recommend',
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    // 2.get请求，并且携带参数
+    // wx.request({
+    //   url: 'http://106.54.54.237:8000/api/hy/home/data',
+    //   data: {
+    //     type: "sell",
+    //     page: 1
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 3.post请求，并且携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   data: {
+    //     name: "vgri",
+    //     age: 22
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   },
+    //   fail: (err) => {
+    //     console.log(err)
+    //   }
+    // })
   }
 })
