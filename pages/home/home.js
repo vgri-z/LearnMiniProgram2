15da91b256285e516209c2c8f3489a1f504a298e
vgri-z <vgri199712@163.com>
2020-02-02 12:21:43 +0800

    组件化开发

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,35 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    counter: 0,
+    isShow: true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleIncrement(event) {
+    console.log('---------', event)
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTabClick(event) {
+    console.log(event.detail)
   },
+  handleSelIncrement() {
+    // 最终目的: 修改my-sel中的counter
+    // 1.获取组件对象
+    const my_sel = this.selectComponent(".sel-class");
+    console.log(my_sel);
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    // 通过setData修改组件中的数据不合理，组件中的数据应由组件自己的方法来修改
+    // my_sel.setData({
+    //   counter: my_sel.data.counter + 5
+    // })
 
+    // 3.通过my-sel里的方法对数据进行修改
+    my_sel.selIncrement(5)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSwitchShow() {
+    this.setData({
+      isShow: !this.data.isShow
+    })
   }
 })
